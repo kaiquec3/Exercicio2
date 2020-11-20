a1b98975c3969e96041bf56ced8e2691e5395022
kaiquec3 <74798358+kaiquec3@users.noreply.github.com>
2020-11-20 19:57:45 -0300

    Add files via upload

---
+++ Exercício 2/Exercício 2/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercício_2
+{
+    class Quadrado
+    {
+        double lado;
+        double area;
+
+        public void SetLado(double p)
+        {
+            lado = p;
+        }
+
+        public double GetLado()
+        {
+            return lado;
+        }
+
+        public double GetArea()
+        {
+            return area;
+        }
+
+        public double CalcularArea()
+        {
+            area = lado * lado;
+            return area;
+        }
+
+    }
+}
---
+++ Exercício 2/Exercício 2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exercício_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a1 = new Quadrado();
+            double k;
+
+            do
+            {
+                Console.Write("Informe o valor do lado: ");
+                k = double.Parse(Console.ReadLine());
+
+                if (k > 0) a1.SetLado(k);
+            }
+            while (k <= 0);
+            
+
+            Console.WriteLine($"A area do quadrado é igual a {a1.CalcularArea()}");
+        }
+    }
+}
